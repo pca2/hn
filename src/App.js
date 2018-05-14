0f4a6890b4aa829d34c52590c45e9d17ea1fa4ec
pca2 <atwaterc@gmail.com>
2018-05-14 17:28:45 -0400

    Move IsNotID oneliner into just param of updatedList

--- src/App.js
+++ src/App.js
@@ -30,8 +30,7 @@ class App extends Component {
     this.onDismiss = this.onDismiss.bind(this);
   }
   onDismiss(id) {
-    const  isNotId = item => item.objectID !== id; 
-    const updatedList = this.state.list.filter(isNotId);
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
   }
 
   render() {
