842ef5a08ed6ec91c087b17d21cc8ee4bed0ed03
pca2 <atwaterc@gmail.com>
2018-05-15 22:36:27 -0400

    Add basic search filter. 1. On input, run onSearchChange 2. onSearchChange sets the form value to the state prop 'SearchTerm' 3. changing sate retriggers render 4. on render we now filter the list based on isSearched 5. isSearched runs includes on list to check T/F if the list contains SearchTerm 6. filter returns just the matching items

--- src/App.js
+++ src/App.js
@@ -21,23 +21,35 @@ const list = [
   },
 ]
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       list,
+      searchTerm: '',
     };
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
   onDismiss(id) {
     const updatedList = this.state.list.filter(item => item.objectID !== id);
     this.setState({ list: updatedList });
   }
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
 
   render() {
     return (
       <div className="App">
-				{this.state.list.map(item =>
+        <form>
+          <input type="text" onChange={this.onSearchChange}/>
+        </form>
+				{this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
